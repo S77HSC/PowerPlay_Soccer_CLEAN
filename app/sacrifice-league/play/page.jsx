52ee49603bb019ce85f7e3e6bea848246673b21d
Simon <info@helpsoccercoach.co.uk>
2025-05-18 12:00:48 +0100

    Fix: allow dynamic rendering for play page (useSearchParams safe)

--- app/sacrifice-league/play/page.jsx
+++ app/sacrifice-league/play/page.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import React, { useEffect, useState, useRef } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { createLeagueTable, updateStandings } from '../../../powerplay-soccer/sacrifice/leagueEngine';
