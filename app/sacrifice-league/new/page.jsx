b8d6736c7cc2a13d4d0a2f0bbc48ab2ce83c84df
Simon <info@helpsoccercoach.co.uk>
2025-05-18 11:54:54 +0100

    Fix naming conflict with dynamic export and import

--- app/sacrifice-league/new/page.jsx
+++ app/sacrifice-league/new/page.jsx
@@ -1,10 +1,10 @@
 'use client';
 export const dynamic = 'force-dynamic';
 
-import dynamic from 'next/dynamic';
+import loadComponent from 'next/dynamic';
 import { Suspense } from 'react';
 
-const TournamentSetup = dynamic(() => import('./TournamentSetup'), { ssr: false });
+const TournamentSetup = loadComponent(() => import('./TournamentSetup'), { ssr: false });
 
 export default function PageWrapper() {
   return (
